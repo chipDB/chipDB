4af59134583e2508c9127a6517fedca53dafc40c
pddivine <pddivine@gmail.com>
2016-11-29 17:55:21 -0800

    Added in create and basic search functionality

--- contracts/EthDb.sol
+++ contracts/EthDb.sol
@@ -8,6 +8,14 @@ contract EthDb {
 
   // // Create
   function create (bytes32[] newRow) {
+    if (tbl.length == 0) { return; }
+    tbl.push(newRow);
+  }
+
+  //Schema
+
+  function createSchema (bytes32[] newRow) {
+    if (tbl.length > 0) { return; }
     tbl.push(newRow);
   }
 
@@ -38,9 +46,36 @@ contract EthDb {
     if (index >= tbl.length) return;
 
     for (uint i = index; i<tbl.length-1; i++){
-        tbl[i] = tbl[i+1];
+      tbl[i] = tbl[i+1];
     }
     delete tbl[tbl.length-1];
     tbl.length--;
   }
+
+  //Search
+
+  function search(bytes32 colName, bytes32 value) returns(bytes32[] x) {
+    int8 index = -1;
+
+    for (uint i = 0; i < tbl[0].length; i++) {
+      if (tbl[0][i] == colName) {
+        index = int8(i);
+      }
+    }
+
+    if (index == -1) { return; }
+
+    for (uint j = 1; j < tbl.length; j++) {
+      if (tbl[j][uint(index)] == value) {
+        for (uint k = 0; k < tbl[j].length; k++) {
+          tblTemp.push(tbl[j][k]);
+        }
+      }
+    }
+
+    x = tblTemp;
+		return x;
+
+  }
+
 }
