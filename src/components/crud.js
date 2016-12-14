80c15d7e21274206a71bf3b6f5af375f8de63a8c
pddivine <pddivine@gmail.com>
2016-12-13 19:45:15 -0800

    Create update and delete functionalit

--- src/components/crud.js
+++ src/components/crud.js
@@ -3,6 +3,7 @@ import {Tabs, Tab} from 'material-ui/Tabs';
 // From https://github.com/oliviertassinari/react-swipeable-views
 import SwipeableViews from 'react-swipeable-views';
 import { web3, ethDb } from '../web3Controller';
+import Form from './form';
 
 const styles = {
   headline: {
@@ -25,15 +26,15 @@ export default class TabsExampleSwipeable extends React.Component {
     };
   }
 
-  _handleCreate(e) {
-    e.preventDefault()
-    this._createRow([this.firstName.value, this.lastName.value, this.address.value]);
-    this.firstName.value = '';
-    this.lastName.value = '';
-    this.address.value = '';
-  }
+  // _handleCreate(arr) {
+  //   this._createRow(arr);
+  // }
 
-  _createRow (arr) {
+  // _handleUpdate(arr) {
+  //   this._updateRow(arr);
+  // }
+
+  _createRow(arr) {
     const self = this;
     const eth = ethDb.deployed();
       eth.create(arr, {from: this.props.mainAccount, gas: 4700000}).then((val) => {
@@ -42,20 +43,25 @@ export default class TabsExampleSwipeable extends React.Component {
       });
   }
 
-  // _readAll () {
-  //   const self = this;
-  //   const eth = ethDb.deployed();
-  //     eth.readAll.call().then((value)=> {
-  //       const result = value.reduce((acc, val, i, arr) => {
-  //         if(i % self.state.tableWidth === 0) acc.push([]);
-  //         acc[acc.length - 1].push(web3.toAscii(val));
-  //         return acc;
-  //       }, []);
-  //       self.setState({tableData: result}, function(){console.log('return statedata', self.state.tableData)});
-  //     });
-  // }
+  _updateRow(arr) {
+    const self = this;
+    const eth = ethDb.deployed();
+    console.log('update', arr);
+    eth.update(...arr, {from: this.props.mainAccount, gas: 4700000}).then((val) => {
+      this.props.readAll();
+    });
+  }
 
-  handleChange = (value) => {
+  _deleteRow (arr) {
+    const self = this;
+    const eth = ethDb.deployed();
+      eth.remove(...arr, {from: this.props.mainAccount, gas: 4700000}).then((val) => {
+        this.props.readAll();
+      });
+  }
+
+
+  _handleChange = (value) => {
     this.setState({
       slideIndex: value,
     });
@@ -65,7 +71,7 @@ export default class TabsExampleSwipeable extends React.Component {
     return (
       <div>
         <Tabs
-          onChange={this.handleChange}
+          onChange={this._handleChange}
           value={this.state.slideIndex}
         >
           <Tab label="Create" value={0} />
@@ -79,25 +85,18 @@ export default class TabsExampleSwipeable extends React.Component {
           <div>
             <h2 style={styles.headline}>Create</h2>
             
-            <form className='SendCoin'>
-              <label htmlFor='first_name'>First Name</label>
-              <input id='first_name' className='RecipientAddress' type='text' ref={(i)=>{ if(i) { this.firstName = i}}} />
-              <label htmlFor='last_name'>Last Name</label>
-              <input id='last_name' className='SendAmount' type='text' ref={(i) => { if(i) { this.lastName = i}}} />
-              <label htmlFor='address'>Address</label>
-              <input id='address' className='SendAmount' type='text' ref={(i) => { if(i) { this.address = i}}} />
-              <br/>
-              <button className='SendBtn' onClick={this._handleCreate.bind(this)}>Send</button>
-            </form>
+            <Form schema={this.props.tableData[0]} handleSubmit={this._createRow.bind(this)}/>
 
 
           </div>
           <div style={styles.slide}>
             <h2 style={styles.headline}>Update</h2>
+            <Form schema={["search_column", "search_value", "column_to_update", "update_value"]} handleSubmit={this._updateRow.bind(this)}/>
             slide n°2
           </div>
           <div style={styles.slide}>
             <h2 style={styles.headline}>Delete</h2>
+            <Form schema={["delete_column", "delete_value"]} handleSubmit={this._deleteRow.bind(this)}/>
             slide n°3
           </div>
         </SwipeableViews>
@@ -105,4 +104,16 @@ export default class TabsExampleSwipeable extends React.Component {
     );
   }
 }
+
+
+          // <form className='SendCoin'>
+          //     <label htmlFor='first_name'>First Name</label>
+          //     <input id='first_name' className='RecipientAddress' type='text' ref={(i)=>{ if(i) { this.firstName = i}}} />
+          //     <label htmlFor='last_name'>Last Name</label>
+          //     <input id='last_name' className='SendAmount' type='text' ref={(i) => { if(i) { this.lastName = i}}} />
+          //     <label htmlFor='address'>Address</label>
+          //     <input id='address' className='SendAmount' type='text' ref={(i) => { if(i) { this.address = i}}} />
+          //     <br/>
+          //     <button className='SendBtn' onClick={this._handleCreate.bind(this)}>Send</button>
+          //   </form>
 
