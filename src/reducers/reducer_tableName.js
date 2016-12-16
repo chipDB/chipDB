42c0da7c41c199ae96246a720b5e346cb7980f54
pddivine <pddivine@gmail.com>
2016-12-16 11:41:30 -0800

    Update reducer to array from string

--- src/reducers/reducer_tableName.js
+++ src/reducers/reducer_tableName.js
@@ -1,13 +1,15 @@
 import { TABLE_NAME } from '../actions/getTableName';
 
 const initialState = {
-  tableName: ''
+  tableName: []
 };
 
 export default function(state = initialState, action) {
   switch (action.type) {
     case TABLE_NAME:
-      return Object.assign({}, state, { tableName: action.payload });
+      const stateClone = Object.assign({}, state);
+      stateClone.tableName.push(action.payload);
+      return stateClone;
     default: return state;
   }
 }
